9fcd72d852bf50f323898c6b44d79a889e8a3c9f
Caleb Sellinger <calebsell24@gmail.com>
2025-02-16 15:21:09 -0600

    Working progress on nb and py scripts

---
+++ sql/create_tables.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS data;
+
+CREATE TABLE data(
+    Community_Area_Name TEXT,
+    Community_Area_Number INTEGER,
+    Property_Type TEXT,
+    Property_Name TEXT,
+    Address TEXT,
+    Zip_Code TEXT,
+    Phone_Number TEXT,
+    Management_Company TEXT,
+    Units TEXT,
+    X_Coordinate INTEGER,
+    Y_Coordinate INTEGER,
+    Latitude INTEGER,
+    Longitude INTEGER,
+    Location TEXT
+);
